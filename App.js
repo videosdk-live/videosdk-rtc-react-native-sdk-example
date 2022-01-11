c61027712aa327556edb16273191be4d7a1f9fc6
Yash Chudasama <yashchudasama1722@gmail.com>
2022-01-11 12:52:31 +0530

    Revert "- Removed Auth Server Dependency"
    This reverts commit 1f923e0d8e8af5178147ced6c1725eacc5170db4.

--- App.js
+++ App.js
@@ -2,21 +2,36 @@ import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native";
 import { MeetingProvider } from "@videosdk.live/react-native-sdk";
 import MeetingContainer from "./src/MeetingContainer";
-import { VIDEOSDK_API, AUTH_TOKEN } from "@env";
+import { REACT_APP_SERVER_URL } from "@env";
 
 export default function App() {
   const [token, setToken] = useState(null);
   const [meetingId, setMeetingId] = useState(null);
-
-  const createMeeting = async (token) => {
+  const getToken = async () => {
     try {
-      const VIDEOSDK_API_ENDPOINT = `${VIDEOSDK_API}/meetings`;
+      const response = await fetch(`${REACT_APP_SERVER_URL}/get-token`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      const { token } = await response.json();
+      return token;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const validateMeeting = async (token) => {
+    try {
+      const VIDEOSDK_API_ENDPOINT = `${REACT_APP_SERVER_URL}/create-meeting`;
       const options = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `${token}`,
         },
+        body: JSON.stringify({ token }),
       };
       const response = await fetch(VIDEOSDK_API_ENDPOINT, options)
         .then(async (result) => {
@@ -31,20 +46,14 @@ export default function App() {
   };
 
   useEffect(async () => {
-    const token = AUTH_TOKEN;
-    const meetingId = await createMeeting(token);
-    console.log(`Meeting Id: ${meetingId}`);
+    const token = await getToken();
+    const meetingId = await validateMeeting(token);
     setToken(token);
     setMeetingId(meetingId);
   }, []);
 
-  return token && meetingId ? (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: "#F6F6FF",
-      }}
-    >
+  return token ? (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F6F6FF" }}>
       <MeetingProvider
         config={{
           meetingId: meetingId,
--- VideosdkRPK.js
+++ VideosdkRPK.js
@@ -1,16 +1,11 @@
-import {
-  NativeModules,
-  NativeEventEmitter
-} from "react-native";
+import { NativeModules, NativeEventEmitter } from "react-native";
 
 class VideosdkRPK extends NativeEventEmitter {
   constructor(nativeModule) {
     super(nativeModule);
 
-    this.startBroadcast = nativeModule.startBroadcast
-
+    this.startBroadcast = nativeModule.startBroadcast;
   }
 }
 
-
-export default new VideosdkRPK(NativeModules.VideosdkRPK)
+export default new VideosdkRPK(NativeModules.VideosdkRPK);
