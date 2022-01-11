d19fe9d2b170d1a11b26d21b77c0dc0aa325d17b
ahmed bhesaniya <65531261+ahmedbhesaniya97@users.noreply.github.com>
2022-01-11 12:45:12 +0530

    Update VideosdkRPK.js

--- VideosdkRPK.js
+++ VideosdkRPK.js
@@ -1,13 +1,10 @@
-import { NativeModules, NativeEventEmitter } from "react-native";
+import { NativeModules, NativeEventEmitter,Platform } from "react-native";
 
 class VideosdkRPK extends NativeEventEmitter {
   constructor(nativeModule) {
     super(nativeModule);
-   
-    this.startBroadcast = nativeModule.startBroadcast;
-
+    this.startBroadcast = Platform.OS === "ios" ? nativeModule.startBroadcast : null
   }
 }
-
 
 export default new VideosdkRPK(NativeModules.VideosdkRPK);
